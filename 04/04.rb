69aecd75d15609bc03b4392ed05ddebd4ac22c6d
Michael Royski <mroyski@gmail.com>
2022-12-04 11:15:24 -0500

    some day 4 tweaks for fun

--- 04/04.rb
+++ 04/04.rb
@@ -33,18 +33,13 @@ overlapping_pairs = 0
 rows.each do |row|
   sections = row.split(',')
 
-  left_nums = sections[0].split('-')
-  right_nums = sections[1].split('-')
+  left = sections[0].split('-').map(&:to_i)
+  right = sections[1].split('-').map(&:to_i)
 
-  left_low = left_nums[0].to_i
-  left_high = left_nums[1].to_i
-  right_low = right_nums[0].to_i
-  right_high = right_nums[1].to_i
-
-  next unless left_low.between?(right_low, right_high) ||
-              left_high.between?(right_low, right_high) ||
-              right_low.between?(left_low, left_high) ||
-              right_high.between?(left_low, left_high)
+  next unless left.first.between?(right.first, right.last) ||
+              left.last.between?(right.first, right.last) ||
+              right.first.between?(left.first, left.last) ||
+              right.last.between?(left.first, left.last)
 
   overlapping_pairs += 1
 end
