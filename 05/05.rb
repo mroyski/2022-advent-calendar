19c71202e563759691daffcfc204abbb95aa99f8
Michael Royski <mroyski@gmail.com>
2022-12-05 18:43:15 -0500

    Day 5

---
+++ 05/05.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+file = File.open('input.txt')
+input = file.read
+
+stacks = {}
+lines = input.split("\n")
+lines.each do |line|
+  break unless line.include?('[')
+
+  crates = line.scan(/.{1,4}/)
+
+  crates.each_with_index do |crate, index|
+    next if crate.strip.empty?
+
+    stacks[index + 1] = [] unless stacks.key?(index + 1)
+    stacks[index + 1].unshift crate[1]
+  end
+end
+
+commands = input.split("\n\n")[1].split("\n")
+
+commands.each do |command|
+  todo = command.split(' ')
+
+  count = todo[1].to_i
+  from = todo[3].to_i
+  to = todo[5].to_i
+
+  #   Part 1
+  #   count.times do
+  #     crate = stacks[from].pop
+  #     stacks[to].push(crate)
+  #   end
+
+  #   Part 2
+  crates = stacks[from].pop(count)
+  stacks[to].concat(crates)
+  stacks[to].flatten
+end
+
+ordered_keys = stacks.keys.sort
+
+result = ''
+ordered_keys.each do |key|
+  result += stacks[key][-1]
+end
+
+# Part 1: FRDSQRRCD
+# Part 2: HRFTQVWNN
+puts result
