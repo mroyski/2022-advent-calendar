9188eb70c9d92651528d2bb0895735b771597c7e
Michael Royski <mroyski@gmail.com>
2022-12-14 18:56:32 -0500

    Day 10 part 2

--- 10/10.rb
+++ 10/10.rb
@@ -12,6 +12,8 @@ end
 file = File.open('input.txt')
 input = file.read.split("\n")
 
+# Part 1
+
 process = []
 cycles = {}
 x = 1
@@ -26,7 +28,7 @@ while process.size > 0
   sum += c * x if [20, 60, 100, 140, 180, 220].include?(c)
 
   p = process[0]
-  
+
   if p.nil?
     process.shift
     c += 1
@@ -45,3 +47,48 @@ end
 
 # 14420
 puts sum
+
+process = input.map do |i|
+  i == 'noop' ? nil : Cycle.new(i.split(' ')[1].to_i)
+end
+
+rows = []
+
+6.times do
+  rows.push([])
+end
+
+rows.each do |r|
+  40.times do
+    r.push '.'
+  end
+end
+
+x = 1
+row = 0
+position = 0
+
+while process.size > 0
+  if [40, 80, 120, 160, 200].include?(position)
+    row += 1
+    position = 0
+  end
+
+  rows[row][position] = '#' if [x - 1, x, x + 1].include?(position)
+
+  if process[0].nil?
+    process.shift
+  else
+    process[0].ticks -= 1
+    if process[0].ticks == 0
+      x += process[0].val
+      process.shift
+    end
+  end
+  position += 1
+end
+
+# RGLRBZAU
+rows.each_with_index do |r, i|
+  puts "#{i}: #{r.join}"
+end
